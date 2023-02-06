e63d4d0ef2c7f8ef8dff32419a1868df4abf7e8d
CheemsBread <dogecheemsbread@gmail.com>
2023-02-06 11:26:37 -0500

    Got the frame of the website working
    It's a simple frame that I can change and update it to make it personalize to me.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
 import logo from './logo.svg';
 import CustomBtn from './components/CustomBtn';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+import Grid from './components/Grid';
 import './App.css';
 
+//changes to imports 
+import SecurityIcon from '@material-ui/icons/Security';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import ImportExportIcon from '@material-ui/icons/ImportExport';
+import ComputerIcon from '@material-ui/icons/Computer';
+import HttpIcon from '@material-ui/icons/Http';
+
+//themes
 const theme = createMuiTheme({
   palette: {
     primary: {
@@ -28,11 +41,47 @@ const theme = createMuiTheme({
   },
 })
 
+const styles = makeStyles({
+  wrapper: {
+    width: "65%",
+    margin: "auto",
+    textAlign: "center"
+  },
+  bigSpace: {
+    marginTop: "5rem"
+  },
+  littleSpace:{
+    marginTop: "2.5rem",
+  },
+  grid:{
+    display: "flex", 
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap", 
+  },
+})
+
+//use "<CustomBtn txt="hello world"/>" for a button.
 function App() {
+  const classes = styles();
+
   return (
     <div className="App">
         <ThemeProvider theme={theme}>
-          <CustomBtn></CustomBtn>
+          <NavBar/>
+
+          <div className={`${classes.grid} ${classes.bigSpace}`}>
+            <Grid icon={<SecurityIcon style={{fill: "#4360A6", height:"125", width:"125"}}/>}  title="Secure" btnTitle="Show me More" />
+            <Grid icon={<EventNoteIcon style={{fill: "#449A76", height:"125", width:"125"}}/>} title="Reliable" btnTitle="Show me More"/>
+            <Grid icon={<TrendingUpIcon style={{fill: "#D05B2D", height:"125", width:"125"}}/>}  title="Performant" btnTitle="Show me More"/>
+          </div>
+
+          <div className={`${classes.grid} ${classes.littleSpace}`}>  
+            <Grid icon={<ImportExportIcon style={{fill: "#5EA780", height:"125", width:"125"}}/>}  title="Modular" btnTitle="Show me More"/>
+            <Grid icon={<ComputerIcon style={{fill: "#E69426", height:"125", width:"125"}}/>}  title="Multi-Platform" btnTitle="Show me More"/>
+            <Grid icon={<HttpIcon style={{fill: "#2EA09D", height:"125", width:"125"}}/>} title="Connected" btnTitle="Show me More"/>
+          </div>
+
         </ThemeProvider>
     </div>
   );
