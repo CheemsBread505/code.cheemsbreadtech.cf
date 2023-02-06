c990d838e3be5a4573fe0e2288d589749fa11b68
CheemsBread <dogecheemsbread@gmail.com>
2023-02-06 01:46:29 -0500

    Got simple web page with button
    Right now it's just a blank web page with a simple button on it. But it's a working react website.
    
    More will be added soon.

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import logo from './logo.svg';
+import CustomBtn from './components/CustomBtn';
+import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main:"#2e1667",
+    },
+    secondary: {
+      main:"#c7d8ed",
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto'
+    ],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: '2rem',
+      },
+    h5: {
+      fontWeight: 100,
+      lineHeight: '2rem',
+    },
+  },
+})
+
+function App() {
+  return (
+    <div className="App">
+        <ThemeProvider theme={theme}>
+          <CustomBtn></CustomBtn>
+        </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
